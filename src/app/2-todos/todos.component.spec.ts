55d696f2c00a8848c0173712c0c52b4688b733de
Brian Phillips <brian.phillips@qq.com>
2017-09-04 13:24:53 +0800

    Providing the Dependencies

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -1,24 +1,20 @@
-/* tslint:disable:no-unused-variable */
+import { HttpModule } from '@angular/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
 
-//NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
-// When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
-
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [ HttpModule ],
+      declarations: [ TodosComponent ],
+      providers: [ TodoService ]
     })
     .compileComponents();
   }));
